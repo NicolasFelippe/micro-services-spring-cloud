7a785b9ece0ec82fd04c72f496f2261d29a0f20d
Nicolas Rocha <nicolas.rocha@agapys.com>
2021-07-10 10:32:59 -0300

    User, Role, many to many association

---
+++ hr-user/src/main/java/com/devsuperior/hruser/entities/Role.java
@@ -0,0 +1,40 @@
+package com.devsuperior.hruser.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roleName;
+
+    public Role() {
+    }
+
+    public Role(Long id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
