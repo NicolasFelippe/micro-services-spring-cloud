6f009a3ec324167ff85be284885e56324a895454
Nicolas Rocha <nicolas.rocha@agapys.com>
2021-07-10 12:53:24 -0300

    Login, jwt token

--- hr-oauth/src/main/java/com/devsuperior/hroauth/resources/AuthResource.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/resources/AuthResource.java
@@ -4,6 +4,7 @@ import com.devsuperior.hroauth.entities.User;
 import com.devsuperior.hroauth.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +20,9 @@ public class AuthResource {
     }
 
     @GetMapping("/search/{email}")
-    public ResponseEntity<User> findByEmail(@PathVariable("email") String email){
+    public ResponseEntity<UserDetails> findByEmail(@PathVariable("email") String email){
         try {
-            return ResponseEntity.ok(userService.findByEmail(email));
+            return ResponseEntity.ok(userService.loadUserByUsername(email));
         }catch (IllegalArgumentException e){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
--- hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -4,11 +4,14 @@ import com.devsuperior.hroauth.entities.User;
 import com.devsuperior.hroauth.feignclients.UserFeignClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -18,13 +21,15 @@ public class UserService {
         this.userFeignClient = userFeignClient;
     }
 
-    public User findByEmail(String email) {
-        User user = userFeignClient.findByEmail(email).getBody();
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userFeignClient.findByEmail(username).getBody();
         if (user == null) {
-            logger.info("Email not found: " + email);
-            throw new IllegalArgumentException("Email not found");
+            logger.info("Email not found: " + username);
+            throw new UsernameNotFoundException("Username not found");
         }
-        logger.info("Email found: " + email);
+        logger.info("Email found: " + username);
         return user;
     }
 }
